7d194a1ac4b110a8ab9410d75665ffda0a59266f
Ahanaf Islam <122388246+ahanaf-Islam-works@users.noreply.github.com>
2023-11-05 17:59:27 +0600

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
-type hello = {
-  hello: string;
-};
-const hello: hello = {
-  hello: "Hello World!",
-};
-console.log(hello);
+import http from "http";
+
+//create a server object:
+http
+  .createServer(function (req, res) {
+    res.write("Hello World!"); //write a response
+    res.end(); //end the response
+  })
+  .listen(3000, function () {
+    console.log("server start at port 3000"); //the server object listens on port 3000
+  });
